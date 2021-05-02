dad956956bd0315c120e5f2bfd1b6ce0c55d1ebb
Leandro Souza <leandrossouza1@gmail.com>
2021-05-02 00:16:43 -0300

    valid request params

--- app/app.py
+++ app/app.py
@@ -1,12 +1,19 @@
-from flask import Flask
-from utils import mahalanobis_algorithm
+from flask import Flask, request
+from utils import mahalanobis_algorithm, valid_params
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    error = valid_params(request)
+
+    if error != None:
+        return error, 404
+
+    # params = prepare_params(request)
+    return request.query_string.decode()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import numpy as np
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 def mahalanobis_algorithm(df, request_dict, weights_dict):
     # handle data
@@ -22,10 +26,43 @@ def mahalanobis_algorithm(df, request_dict, weights_dict):
 
     weight = np.diag(weights[request_keys].values[0])
 
-    # applying mahalanobis algorithm 
+    # applying mahalanobis algorithm
     distances = (pd.DataFrame(abs(np.diag(
         (data_arr - query_arr) @ weight @ np.linalg.inv(np.cov(data_arr.transpose())) @ (
             ((data_arr - query_arr) @ weight).transpose()))) ** .5)).sort_values(by=0)
     newdistances = pd.DataFrame(distances)
     sorted_values = newdistances.sort_values(by=0)
     return sorted_values
+
+
+def valid_params(request):
+    value = request.args.get('value')
+    rooms = request.args.get('rooms')
+    area = request.args.get('area')
+
+    if not value:
+        return 'value is a required field'
+
+    if not is_int(value):
+        return 'value must be a integer'
+
+    if not rooms:
+        return 'rooms is a required field'
+
+    if not is_int(rooms):
+        return 'rooms must be a integer'
+
+    if not area:
+        return 'area is a required field'
+
+    if not is_int(area):
+        return 'area must be a integer'
+
+    return None
+
+def is_int(x):
+    try:
+        int(x)
+        return True
+    except:
+        return False
